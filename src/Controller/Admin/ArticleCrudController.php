ba95618efe2c168f3018b1c901da8536cacc29b0
0xArthem <82900721+0xArthem@users.noreply.github.com>
2023-03-26 20:33:13 +0200

    11 - Ajout de propriétés à Article pour une section vocabulaire

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -3,15 +3,16 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
+
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 
 class ArticleCrudController extends AbstractCrudController
 {
@@ -36,6 +37,19 @@ class ArticleCrudController extends AbstractCrudController
             TextField::new('title')->setLabel('Titre'),
             TextField::new('slug')->setLabel('Slug'),
             TextEditorField::new('description')->setLabel('Description'),
+            TextEditorField::new('content')->setLabel('Contenu'),
+            /** vocabulaire */
+            TextEditorField::new('vocabulaire1')->hideOnIndex()->setLabel('Vocabulaire n°1'),
+            TextEditorField::new('vocabulaire2')->hideOnIndex()->setLabel('Vocabulaire n°2'),
+            TextEditorField::new('vocabulaire3')->hideOnIndex()->setLabel('Vocabulaire n°3'),
+            TextEditorField::new('vocabulaire4')->hideOnIndex()->setLabel('Vocabulaire n°4'),
+            TextEditorField::new('vocabulaire5')->hideOnIndex()->setLabel('Vocabulaire n°5'),
+            TextEditorField::new('vocabulaire6')->hideOnIndex()->setLabel('Vocabulaire n°6'),
+            TextEditorField::new('vocabulaire7')->hideOnIndex()->setLabel('Vocabulaire n°7'),
+            TextEditorField::new('vocabulaire8')->hideOnIndex()->setLabel('Vocabulaire n°8'),
+            TextEditorField::new('vocabulaire9')->hideOnIndex()->setLabel('Vocabulaire n°9'),
+            TextEditorField::new('vocabulaire10')->hideOnIndex()->setLabel('Vocabulaire n°10'),
+            /** fin vocabulaire */
             TextField::new('image')->hideOnIndex()->setLabel('Image'),
             AssociationField::new('niveau')->setLabel('Niveau'),
             AssociationField::new('theme')->setLabel('Thème'),
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -2,9 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\ArticleRepository;
 use DateTime;
+use App\Entity\Theme;
+use App\Entity\Niveau;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ArticleRepository;
 
 /**
  * @ORM\Entity(repositoryClass=ArticleRepository::class)
@@ -57,6 +59,61 @@ class Article
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $slug;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $vocabulaire1;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $vocabulaire2;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $vocabulaire3;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $vocabulaire4;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $vocabulaire5;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $vocabulaire6;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $vocabulaire7;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $vocabulaire8;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $vocabulaire9;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $vocabulaire10;
 
     public function __construct()
     {
@@ -160,6 +217,192 @@ class Article
     public function setSlug(?string $slug): self
     {
         $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getVocabulaire1(): ?string
+    {
+        return $this->vocabulaire1;
+    }
+
+    public function setVocabulaire1(?string $vocabulaire1): self
+    {
+        $this->vocabulaire1 = $vocabulaire1;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of vocabulaire10
+     */
+    public function getVocabulaire10()
+    {
+        return $this->vocabulaire10;
+    }
+
+    /**
+     * Set the value of vocabulaire10
+     */
+    public function setVocabulaire10($vocabulaire10): self
+    {
+        $this->vocabulaire10 = $vocabulaire10;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of vocabulaire9
+     */
+    public function getVocabulaire9()
+    {
+        return $this->vocabulaire9;
+    }
+
+    /**
+     * Set the value of vocabulaire9
+     */
+    public function setVocabulaire9($vocabulaire9): self
+    {
+        $this->vocabulaire9 = $vocabulaire9;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of vocabulaire8
+     */
+    public function getVocabulaire8()
+    {
+        return $this->vocabulaire8;
+    }
+
+    /**
+     * Set the value of vocabulaire8
+     */
+    public function setVocabulaire8($vocabulaire8): self
+    {
+        $this->vocabulaire8 = $vocabulaire8;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of vocabulaire7
+     */
+    public function getVocabulaire7()
+    {
+        return $this->vocabulaire7;
+    }
+
+    /**
+     * Set the value of vocabulaire7
+     */
+    public function setVocabulaire7($vocabulaire7): self
+    {
+        $this->vocabulaire7 = $vocabulaire7;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of vocabulaire6
+     */
+    public function getVocabulaire6()
+    {
+        return $this->vocabulaire6;
+    }
+
+    /**
+     * Set the value of vocabulaire6
+     */
+    public function setVocabulaire6($vocabulaire6): self
+    {
+        $this->vocabulaire6 = $vocabulaire6;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of vocabulaire5
+     */
+    public function getVocabulaire5()
+    {
+        return $this->vocabulaire5;
+    }
+
+    /**
+     * Set the value of vocabulaire5
+     */
+    public function setVocabulaire5($vocabulaire5): self
+    {
+        $this->vocabulaire5 = $vocabulaire5;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of vocabulaire4
+     */
+    public function getVocabulaire4()
+    {
+        return $this->vocabulaire4;
+    }
+
+    /**
+     * Set the value of vocabulaire4
+     */
+    public function setVocabulaire4($vocabulaire4): self
+    {
+        $this->vocabulaire4 = $vocabulaire4;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of vocabulaire3
+     */
+    public function getVocabulaire3()
+    {
+        return $this->vocabulaire3;
+    }
+
+    /**
+     * Set the value of vocabulaire3
+     */
+    public function setVocabulaire3($vocabulaire3): self
+    {
+        $this->vocabulaire3 = $vocabulaire3;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of vocabulaire2
+     */
+    public function getVocabulaire2()
+    {
+        return $this->vocabulaire2;
+    }
+
+    /**
+     * Set the value of vocabulaire2
+     */
+    public function setVocabulaire2($vocabulaire2): self
+    {
+        $this->vocabulaire2 = $vocabulaire2;
 
         return $this;
     }
