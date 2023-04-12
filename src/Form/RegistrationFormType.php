153010134ee04c93f5939f6bd8b07d2265232cff
0xArthem <82900721+0xArthem@users.noreply.github.com>
2023-04-12 10:55:48 +0200

    28 - Ajout des contraintes sur le formulaire d'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,15 +4,17 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
@@ -20,53 +22,63 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class, [
+        ->add('email', EmailType::class, [
+            'label' => false,
+            'attr' => [
+                'placeholder' => 'Adresse électronique'
+            ],
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Veuillez entrer une adresse électronique.',
+                ]),
+                new Email([
+                    'message' => 'Veuillez entrer une adresse électronique valide.',
+                ]),
+            ],
+        ])
+        ->add('agreeTerms', CheckboxType::class, [
+            'mapped' => false,
+            'label' => 'J\'accepte le traitement de mes données.',
+            'constraints' => [
+                new IsTrue([
+                    'message' => 'Vous devez accepter notre politique de traitement de vos données.',
+                ]),
+            ],
+        ])
+        ->add('plainPassword', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'mapped' => false,
+            'attr' => ['autocomplete' => 'new-password'],
+            'invalid_message' => 'Les mots de passe doivent correspondre.',
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Veuillez entrer un mot de passe.',
+                ]),
+                new Length([
+                    'min' => 6,
+                    'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères.',
+                    'max' => 4096,
+                ]),
+                new Regex([
+                    'pattern' => '/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9\W]).*$/',
+                    'message' => 'Votre mot de passe doit contenir au moins une majuscule, une minuscule, et un caractère spécial ou un chiffre.',
+                ]),
+            ],
+            'first_options' => [
                 'label' => false,
                 'attr' => [
-                    'placeholder' => 'Adresse électronique'
+                    'placeholder' => 'Mot de passe',
+                    'class' => 'form-control mt-4'
                 ]
-            ])
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'label' => 'J\'accepte le traitement de mes données.',
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'Vous devez accepter notre politique de traitement de vos données.',
-                    ]),
+            ],
+            'second_options' => [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Confirmez le mot de passe',
+                    'class' => 'form-control mt-4'
                 ]
-            ])
-        ->add('plainPassword', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'invalid_message' => 'Les mots de passe doivent correspondre.',
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Veuillez entrer un mot de passe.',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères.',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-                'first_options' => [
-                    'label' => false,
-                    'attr' => [
-                        'placeholder' => 'Mot de passe',
-                        'class' => 'form-control mt-4'
-                    ]
-                ],
-                'second_options' => [
-                    'label' => false,
-                    'attr' => [
-                        'placeholder' => 'Confirmez le mot de passe',
-                        'class' => 'form-control mt-4'
-                    ]
-                ],
-            ])
-        ;
+            ]
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
