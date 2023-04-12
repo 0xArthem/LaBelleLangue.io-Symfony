ff3754172a150a1dc9eebc8ae276db8cf32b83fb
0xArthem <82900721+0xArthem@users.noreply.github.com>
2023-04-12 15:57:39 +0200

    36 - Forum : Formulaire d'envoi de messages sur un topic et affichage des données (message, pseudo, heure)

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Form\MessageType;
 use App\Repository\TopicRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 
 class ForumController extends AbstractController
 {
@@ -24,15 +28,36 @@ class ForumController extends AbstractController
     /**
      * @Route("/forum/{slug}", name="topic")
      */
-    public function topic(TopicRepository $topicRepository, $slug): Response
+    public function topic(TopicRepository $topicRepository, $slug, Request $request, Security $security): Response
     {
         $topic = $topicRepository->findOneBy(['slug' => $slug]);
 
         $messages = $topic->getMessages();
 
+        $message = new Message();
+        // Attribution du Topic au message, et de l'utilisateur
+        $message->setTopic($topic);
+        $message->setUser($security->getUser());
+
+        // Création du formulaire
+        $form = $this->createForm(MessageType::class, $message);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Sauvegarde du message dans la base de données
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($message);
+            $entityManager->flush();
+
+            // Redirection vers la page du topic
+            return $this->redirectToRoute('topic', ['slug' => $slug]);
+        }
+
         return $this->render('forum/topic.html.twig', [
             'topic' => $topic,
-            'messages' => $messages
+            'messages' => $messages,
+            'form' => $form->createView()
         ]);
     }
 }
